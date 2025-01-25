c1c764c7886e374fa49a43ae55e12fba29067fe5
星海猎人07 <2058626192@qq.com>
2025-01-25 01:43:13 +0000

    删除文件 xuecheng-plus-auth/src/main/java/com/xuecheng/auth

--- xuecheng-plus-auth/src/main/java/com/xuecheng/auth/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.xuecheng.auth.service;
-
-import com.xuecheng.ucenter.model.dto.AuthParamsDto;
-import com.xuecheng.ucenter.model.dto.XcUserExt;
-
-public interface AuthService {
-
-    XcUserExt execute(AuthParamsDto authParamsDto);
-}
